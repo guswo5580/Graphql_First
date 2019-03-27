180771d2a420edae5641543dd3fb3c4dd1a8e40e
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2019-03-27 21:47:49 +0900

    complete schema and db resolvers file

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,12 @@
+import { getMovies, getMovie, getSuggestions } from './db';
 //schema에서 선언한 쿼리에 대한 대답을 정하는 부분
-const Rick = {
-    name : 'Rick',
-    age : 26,
-    gender : 'male'
-};
+
 const resolvers = {
     Query : {
-        person : () => Rick 
-    }
+        movies : (_, { limit , rating  }) => getMovies(limit, rating),
+        movie : (_, {id} ) => getMovie(id),
+        suggestions : (_, {id} ) => getSuggestions(id)
+    },
 };
 
 export default resolvers;
